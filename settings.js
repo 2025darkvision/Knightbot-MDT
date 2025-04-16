bac6e38dd3274082c7553d107fbb3b4e4e77d92e
2025darkvision <thaboamin@gmail.com>
2025-04-16 11:32:54 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: '2025darkvision', // Your name
   ownerNumber: '254751071939', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
